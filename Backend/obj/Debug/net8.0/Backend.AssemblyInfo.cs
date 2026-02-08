dff65f338479c8589dcf650776e09b0708daee7c
Mads Hansen <2100mads@live.dk>
2026-02-08 10:45:39 +0100

    Generated

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371244ae967bf49845bd51d6df08c291dbeec8c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c926f46aabfe9cb36f6fb4dfb3d657d469c9db54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
